d5ae44e2cc3a5cc8112e5ae0132e75a0b4bca1bc
Tom Österlund <tom.osterlund1@gmail.com>
2023-01-03 16:27:24 +0100

    feat(core): write logic for an accessible custom select

--- packages/core/src/fields/MultiSelect.ts
+++ packages/core/src/fields/MultiSelect.ts
@@ -8,6 +8,8 @@ import InputLabel from "../components/input-label";
 export class MultiSelect extends FormField {
   options: MultiSelectOption[] = []
   optionsElement: HTMLDivElement | null = null
+  listItemElements: HTMLLIElement[] = []
+  listItemIds: string[] = []
 
   constructor(
     name: string,
@@ -19,26 +21,32 @@ export class MultiSelect extends FormField {
   }
 
   render(mountingElement: HTMLElement) {
-    const inputLabel = new InputLabel(this).render()
-
-    const elements: (VNode|null)[] = [inputLabel, this.getSelectVNode(this.optionsVNodes)]
-
-    this.projector.append(mountingElement, () => {
-      return h('div', { id: this.id, class: this.wrapperClasses }, elements)
-    })
-
+    const elements = this.getSelectElements();
+    this.projector.append(mountingElement, () => this.getSelectWrapper(elements))
     this.saveDOMElements();
   }
 
-  private saveDOMElements() {
-    this.optionsElement = document.getElementById(this.optionsElementID) as HTMLDivElement
-    this.inputDOMElement = document.getElementById(this.inputId) as HTMLInputElement
+  private getSelectWrapper(elements: (VNode | null)[]) {
+    return h('div', {id: this.id, class: this.wrapperClasses}, elements);
   }
 
-  getSelectVNode(options: VNode) {
+  private getSelectElements() {
+    return [
+      new InputLabel(this, this.labelId).render(),
+      this.getSelectVNode(this.optionsVNodes)
+    ];
+  }
+
+  get labelId() {
+    return `phlib__select-label-${this.name}`
+  }
+
+  private getSelectVNode(options: VNode) {
     return h(
       'div',
       { class: 'phlib__select-wrapper' },
+      // 1. First, we render a readonly input field, which is used to display the selected option
+      // This would be the mock implementation of the select-tag
       [
         h(
           'input',
@@ -46,12 +54,33 @@ export class MultiSelect extends FormField {
             placeholder: this.placeholder,
             value: String(this.selectedOptionLabel),
             readOnly: true,
+            'aria-labelledby': this.labelId,
+            role: 'button',
+            onkeydown: event => this.handleOnKeydownForInput(event),
             ...this._getGlobalInputProperties(),
           }
         ),
+        // 2. Then, we render a ul-element, which contains the mock-options, rendered through li-tags
         options
       ]
     )
+  }
+
+  private handleOnKeydownForInput(event: KeyboardEvent) {
+    if (event.code === 'Space' || event.code === 'Enter') {
+      this.displayOptions()
+    }
+  }
+
+  /**
+   * Save all DOM elements, that need to be manipulated when opening/closing the options, and when selecting an option
+   * */
+  private saveDOMElements() {
+    this.optionsElement = document.getElementById(this.optionsElementID) as HTMLDivElement
+    this.inputDOMElement = document.getElementById(this.inputId) as HTMLInputElement
+    this.listItemIds.forEach((id, index) => {
+      this.listItemElements.push(document.getElementById(id) as HTMLLIElement)
+    })
   }
 
   get selectedOptionLabel() {
@@ -61,27 +90,104 @@ export class MultiSelect extends FormField {
   get optionsVNodes() {
     return h(
       'ul',
-      { class: 'phlib__select-options', id: this.optionsElementID },
-      this.options.map(option => {
+      { class: 'phlib__select-options', id: this.optionsElementID, tabIndex: 0, role: 'list', ariaExpanded: false },
+      this.options.map((option, index) => {
+        const optionId = 'phlib__select-option-' + this.name + '-' + index
+        this.listItemIds.push(optionId)
+
         return h(
           'li',
-          { onclick: this.optionClickHandler.bind(this), 'data-value': option.value },
+          {
+            onclick: this.selectOption.bind(this),
+            onkeydown: this.optionKeyDownHandler.bind(this),
+            'data-value': option.value,
+            tabIndex: 0,
+            id: optionId,
+          },
           [option.label])
       })
     )
   }
 
   /**
-   * Is executed, when an option is clicked
+   * Is executed, when an option is selected via mouse or keyboard
    * */
-  optionClickHandler(event: MouseEvent) {
-    const target = event.target as HTMLElement
-    const selectedValue = target.dataset.value
+  selectOption(event: MouseEvent|KeyboardEvent) {
+    const selectedValue = (event.target as HTMLLIElement).dataset.value
+
     if (selectedValue) {
       this.setValue(selectedValue);
       (this.inputDOMElement as HTMLInputElement).value = this.options.find(option => option.value === selectedValue)?.label || ''
     }
+
     this.hideOptions()
+  }
+
+  /**
+   * Is executed, when an option is selected via keyboard
+   * */
+  optionKeyDownHandler(e: KeyboardEvent) {
+    switch (e.code) {
+      case 'Enter':
+        this.selectOption(e);
+        this.hideOptions()
+        return;
+
+      case 'ArrowDown':
+        this.focusNextListItem(e.code);
+        return;
+
+      case 'ArrowUp':
+        this.focusNextListItem(e.code);
+        return;
+
+      case 'Escape':
+        this.hideOptions()
+        return;
+
+      default:
+        return;
+    }
+  }
+
+  private focusNextListItem(direction?: string) {
+    if (!document.activeElement?.id) return;
+
+    const currentActiveElementIndex = this.listItemIds.indexOf(document.activeElement.id);
+
+    if (direction === 'ArrowDown') {
+      this.navigateOnDownArrowKey(currentActiveElementIndex);
+    } else if (direction === 'ArrowUp') {
+      this.navigateOnUpArrowKey(currentActiveElementIndex);
+    }
+  }
+
+  private navigateOnUpArrowKey(currentActiveElementIndex: number) {
+    const currentActiveElementIsFirstItem = currentActiveElementIndex === 0;
+
+    if (currentActiveElementIsFirstItem) return
+
+    const nextListItemId = this.listItemIds[currentActiveElementIndex - 1];
+    const nextItem = document.querySelector(`#${nextListItemId}`);
+    if (nextItem instanceof HTMLLIElement) nextItem.focus();
+  }
+
+  private navigateOnDownArrowKey(currentActiveElementIndex: number) {
+    const currentActiveElementIsLastItem = currentActiveElementIndex >= this.listItemIds.length - 1;
+
+    if (currentActiveElementIsLastItem) return;
+
+    const nextListItemId = this.listItemIds[currentActiveElementIndex + 1];
+    const nextItem = document.querySelector(`#${nextListItemId}`);
+
+    if (nextItem instanceof HTMLLIElement) nextItem.focus();
+  }
+
+  private focusFirstOption() {
+    if (document.activeElement?.id !== this.inputId) return
+
+    const firstListItem = document.getElementById(this.listItemIds[0])
+    if (firstListItem) setTimeout(() => firstListItem.focus(), 10)
   }
 
   get optionsElementID() {
@@ -89,19 +195,36 @@ export class MultiSelect extends FormField {
   }
 
   private displayOptions() {
-    (this.optionsElement as HTMLDivElement).style.display = 'block'
+    if (!(this.optionsElement instanceof HTMLUListElement)) return
+
+    this.optionsElement.style.display = 'block'
+    this.optionsElement.ariaExpanded = 'true'
+    this.focusFirstOption()
+    document.addEventListener('click', this.detectClickOutside.bind(this))
+  }
+
+  private detectClickOutside(event: MouseEvent) {
+    if (!(event.target instanceof HTMLElement)) return
+
+    const clickedOutsideSelect = !event.target.closest(`#${this.id}`)
+
+    if (clickedOutsideSelect && (this.optionsElement as HTMLDivElement).style.display === 'block') {
+      this.hideOptions()
+      document.removeEventListener('click', this.detectClickOutside.bind(this))
+    }
   }
 
   private hideOptions() {
-    (this.optionsElement as HTMLDivElement).style.display = 'none'
+    if (!(this.optionsElement instanceof HTMLUListElement)) return
+
+    this.optionsElement.style.display = 'none'
+    this.optionsElement.ariaExpanded = 'false'
   }
 
   _onClick(event: Event) {
     if (!(this.optionsElement instanceof HTMLElement)) return
 
-    if (this.optionsElement.style.display === 'block') this.hideOptions()
-    else this.displayOptions();
-
+    this.displayOptions();
     super._onClick(event);
   }
 }

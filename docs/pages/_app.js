7c0a3cc5fb7ebcb6b2ceb8501a7ae5dbf2977e6a
Tom Österlund <tom.osterlund1@gmail.com>
2023-01-05 20:02:13 +0100

    chore(docs): remove documentation directory, which has been migrated to new repo

--- docs/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '../node_modules/@phormal/theme-basic/dist/index.css'
-import '../node_modules/@phormal/theme-material/dist/index.css'
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}

5753aaaad7d89bdd763b407e9f69609107fd4a5d
Tom Österlund <tom.osterlund1@gmail.com>
2023-01-11 23:17:05 +0100

    feat: add translations for all 12 first supported languages

---
+++ tests/unit/language/index.spec.ts
@@ -0,0 +1,74 @@
+import {describe, expect, it} from "vitest";
+import useEmail from "@phormal/use-email/src";
+import useLength from "@phormal/use-length/src";
+import useRegex from "@phormal/use-regex/src";
+import useRequired from "@phormal/use-required/src";
+import useValidZip from "@phormal/use-valid-zip/src";
+import {ErrorMessageObject} from "@phormal/core/src/types/globals";
+
+const allSupportedLanguages = [
+  'en', // English
+  'ru', // Russian
+  'es', // Spanish
+  'tr', // Turkish
+  // 'fa', // Persian
+  'fr', // French
+  'de', // German
+  'ja', // Japanese
+  'it', // Italian
+  'pt', // Portuguese
+  'zh', // Chinese
+  'vi', // Vietnamese
+];
+
+const testThatAllLanguagesAreTranslated = (errorMessages: ErrorMessageObject[]) => {
+  for (const errorMessage of errorMessages) {
+    for (const language of allSupportedLanguages) {
+      if (!errorMessage[language]) {
+        throw new Error(`Language "${language}" is not translated for error message where "en" === "${errorMessage.en}"`);
+      }
+
+      expect(errorMessage[language]).toBeDefined()
+    }
+  }
+}
+
+describe('Translations objects', () => {
+  it('Has a translation for all languages  in in use-email', () => {
+    const errorMessages = [
+      useEmail().errorMessages?.email as ErrorMessageObject
+    ]
+    testThatAllLanguagesAreTranslated(errorMessages)
+  })
+
+  it('Has a translation for all languages in use-length', () => {
+    const errorMessages = [
+      useLength(1).errorMessages?.length as ErrorMessageObject,
+      useLength(1, 2).errorMessages?.minLength as ErrorMessageObject,
+      useLength(null, 2).errorMessages?.maxLength as ErrorMessageObject,
+    ]
+    testThatAllLanguagesAreTranslated(errorMessages)
+  })
+
+  it('Has a translation for all languages in use-regex', () => {
+    const errorMessages = [
+      useRegex(/.*/).errorMessages?.regex as ErrorMessageObject,
+      useRegex(/.*/, 'XXX').errorMessages?.regexReadableFormat as ErrorMessageObject,
+    ]
+    testThatAllLanguagesAreTranslated(errorMessages)
+  })
+
+  it('Has a translation for all languages in use-required', () => {
+    const errorMessages = [
+      useRequired().errorMessages?.required as ErrorMessageObject,
+    ]
+    testThatAllLanguagesAreTranslated(errorMessages)
+  })
+
+  it('Has a translation for all languages in use-valid-zip', () => {
+    const errorMessages = [
+      useValidZip().errorMessages?.zip as ErrorMessageObject,
+    ]
+    testThatAllLanguagesAreTranslated(errorMessages)
+  })
+})

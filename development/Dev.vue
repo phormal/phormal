d169176bc6c0347589c47bc248c8e4bfb640547c
Tom Österlund <tom.osterlund1@gmail.com>
2023-01-23 07:57:23 +0100

    feat: add support for dark-mode through style-attribute color-scheme

--- development/Dev.vue
+++ development/Dev.vue
@@ -138,8 +138,8 @@ export default {
       el: '#phormal',
       validation: 'active',
       language: 'de',
-      theme: 'material',
-      // theme: 'basic',
+      // theme: 'material',
+      theme: 'basic',
     })
 
     this.values = this.form.$values()

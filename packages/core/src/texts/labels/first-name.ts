fec7a2a5269f496bb9b3525e5269f099fc115a9f
Tom Österlund <63011648+tomosterlund@users.noreply.github.com>
2023-01-26 05:52:50 +0100

    feat(core): add label texts for address forms (#43)
    * feat(core): add texts for address form labels
    
    * chore: bump dev-dependencies

---
+++ packages/core/src/texts/labels/first-name.ts
@@ -0,0 +1,14 @@
+export const translationFirstName = {
+  en: 'First name',
+  de: 'Vorname',
+  fr: 'Prénom',
+  es: 'Primer nombre',
+  it: 'Nome di battesimo',
+  pt: 'Primeiro nome',
+  ru: 'Имя',
+  ja: 'ファーストネーム',
+  zh: '名',
+  vi: 'Tên đầu tiên',
+  fa: 'نام کوچک',
+  tr: 'İlk adı',
+}
